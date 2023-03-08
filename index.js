56ffecf8ceb1c7aef717085e2e1bdc8d72160163
Orelso <orelso25@gmail.com>
2023-03-08 10:59:50 +0200

    final

--- index.js
+++ index.js
@@ -1,44 +1,63 @@
-
 function ask() {
-    // let userName = "";
+  // let userName = "";
 
-    // userName ? console.log('Hello Jane') : console.log('Hello');
+  // userName ? console.log('Hello Jane') : console.log('Hello');
 
-    userQuestion = 'Will I make it?';
-    console.log(userQuestion);
+  userQuestion = "Will I make it?";
+  console.log(userQuestion);
 
-    randomNumber = Math.floor(Math.random() * 8);
-    console.log(randomNumber);
+  randomNumber = Math.floor(Math.random() * 8);
+  console.log(randomNumber);
 
-    eightBall = '';
-    let result
-switch (randomNumber) {
+  eightBall = "";
+  let result;
+  switch (randomNumber) {
     case 0:
-        eightBall = 'It is certain';
-        break;
+      eightBall = "It is certain";
+      break;
     case 1:
-        eightBall = 'It is decidedly so';
-        break;
+      eightBall = "It is decidedly so";
+      break;
     case 2:
-        eightBall = "I'm not sure try again";
-        break;
+      eightBall = "I'm not sure try again";
+      break;
     case 3:
-        eightBall = 'Cannot predict right now';
-        break;
+      eightBall = "Cannot predict right now";
+      break;
     case 4:
-        eightBall = 'Do not count on it';
-        break;
+      eightBall = "Do not count on it";
+      break;
     case 5:
-        eightBall = 'My sources say no';
-        break;
+      eightBall = "My sources say no";
+      break;
     case 6:
-        eightBall = 'The outlook is not so good';
-        break;
+      eightBall = "The outlook is not so good";
+      break;
     case 7:
-        eightBall = 'The signs point to yes';
+      eightBall = "The signs point to yes";
     default:
-        break;
+      break;
+  }
+  console.log(eightBall);
+  document.getElementById("eightBall").textContent = eightBall;
 }
-console.log(eightBall);
-document.getElementById('eightBall').textContent = eightBall;
+/* -------------------------------------------------------------------------------------------------------------------------------------------(Modal)------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+function showModal(message) {
+  var modalContainer = document.getElementById("modal-container");
+  var modalMessage = document.getElementById("modal-message");
+  modalMessage.textContent = message;
+  modalContainer.style.display = "block";
+
+  var closeButtons = document.getElementsByClassName("close");
+  for (var i = 0; i < closeButtons.length; i++) {
+    closeButtons[i].addEventListener("click", function () {
+      modalContainer.style.display = "none";
+    });
+  }
 }
+
+window.onload = function () {
+  showModal(
+    "Ask the Magic 8 Ball a question outloud. It will give you your answer."
+  );
+};
